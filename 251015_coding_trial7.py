900c956f7e9389a51422fd13d3e2294b2659f95d
Harim-Yoo <129376024+Harim-Yoo@users.noreply.github.com>
2025-10-15 18:48:30 +0900

    Update 251015_coding_trial7.py

--- 251015_coding_trial7.py
+++ 251015_coding_trial7.py
@@ -9,6 +9,18 @@ expr_3 = x/y
 
 eqtn_1 = sp.Eq(expr_1,expr_2)
 eqtn_2 = sp.Eq(expr_2,expr_3)
-result = sp.solve([eqtn_1,eqtn_2],[x,y])
+result = sp.solve([eqtn_1,eqtn_2],[x,y]) #multiple variable일때
 
 print(result)
+
+----------------------------------------------------------------------
+
+import sympy as sp
+
+x = sp.Symbol('x',real=True)
+
+expr_1 = abs(x+3)
+expr_2 = 3*abs(x-2)
+eq = sp.Eq(expr_1,expr_2)
+result = sp.solve(eq,x)
+print(sum(result))
