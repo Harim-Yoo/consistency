5c2f7ccde173ffaf314c3671aa727d44d8b3a4d8
Harim-Yoo <129376024+Harim-Yoo@users.noreply.github.com>
2025-10-21 19:13:36 +0900

    Update 251021_coding_trial3.py

--- 251021_coding_trial3.py
+++ 251021_coding_trial3.py
@@ -15,3 +15,24 @@ dfs(0,[])
 print(res)
 print(len(res))
     
+---------------------------------------------------------------
+
+import math 
+
+nums = list(range(1,13))
+res = []
+
+def dfs(start,path):
+    if len(path) == 4:
+        if sum(path)==24:
+            if math.prod(path)%2==0:
+                res.append(path.copy())
+        return None
+    for i in range(start,len(nums)):
+        path.append(nums[i])
+        dfs(i+1,path)
+        path.pop()
+
+dfs(0,[])
+print(res)
+print(len(res))
