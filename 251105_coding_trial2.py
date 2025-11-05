d311f41060a4b2c88576f032b4802b40c5b586fb
Harim-Yoo <129376024+Harim-Yoo@users.noreply.github.com>
2025-11-05 15:18:22 +0900

    Create 251105_coding_trial2.py

---
+++ 251105_coding_trial2.py
@@ -0,0 +1,14 @@
+N = int(input())
+nums = map(int,input().split())
+print(list(nums))
+
+--------------------------------
+
+N = int(input())
+res = []
+
+for _ in range(N):
+    val = int(input())
+    res.append(val)
+    
+print(res)
